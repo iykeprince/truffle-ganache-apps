b65a0205bc1e4b54de792b528c617c22a352d50e
Ikechukwu Israel <iykeprince48@gmail.com>
2022-03-07 22:21:22 +0100

    commiting basics of smart contract development with truffle

---
+++ basics/contracts/Helloworld.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.8.12;
+
+contract Helloworld {
+    function hello() public pure returns (string memory) {
+        return "hello, welcome back to solidity. This is only but a test";
+    }
+}
