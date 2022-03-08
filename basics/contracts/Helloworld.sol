676e07072a8eb3f32984fb133422216a22bac7e8
Ikechukwu Israel <iykeprince48@gmail.com>
2022-03-08 01:38:00 +0100

    deploying to local in a more constructive form

--- basics/contracts/Helloworld.sol
+++ basics/contracts/Helloworld.sol
@@ -1,7 +1,19 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.12;
 
 contract Helloworld {
+    string public message;
+
+    constructor(string memory _message) {
+        message = _message;
+    }
+
     function hello() public pure returns (string memory) {
         return "hello, welcome back to solidity. This is only but a test";
+    }
+
+    function setMessage(string memory _message) public payable {
+        require(msg.value >= 1 ether, "Condition not met");
+        message = _message;
     }
 }
--- basics/migrations/2_hello_world_migration.js
+++ basics/migrations/2_hello_world_migration.js
@@ -1,5 +1,10 @@
 const Helloworld = artifacts.require("Helloworld");
+const PermanentHello = artifacts.require("PermanentHello");
 
 module.exports = function (deployer) {
-  deployer.deploy(Helloworld);
+  deployer.deploy(Helloworld, "Hello world constructor").then(async () => {
+    const instance = await Helloworld.deployed();
+    const message = instance.message();
+    return deployer.deploy(PermanentHello, message);
+  });
 };
